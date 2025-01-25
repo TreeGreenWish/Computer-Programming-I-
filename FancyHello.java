f471e27ff66808950eb754d39550aca86834c0e2
Tyler McCardle <68253755+TreeGreenWish@users.noreply.github.com>
2025-01-25 17:56:09 -0600

    Add files via upload

---
+++ FancyHello.java
@@ -0,0 +1,14 @@
+public class FancyHello {
+   public static void main (String[] args) {
+      hello();
+      hello();
+      hello();
+   }
+   
+   public static void hello() {
+      System.out.println("///************\\\\\\");
+      System.out.println("|||hello world!|||");
+      System.out.println("\\\\\\************///");
+      System.out.println();
+   }
+}
---
+++ MyFirstProgram.java
@@ -0,0 +1,20 @@
+/*
+Student Name: Tyler McCardle
+Assignment #1: MyFirstProgram
+Assignment Description: modify an existing program
+Issue Date: 1/21/2025
+Due Date: 1/26/2025
+Course: COSC 1336-020
+Instructor: Frank Yang
+*/
+
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("I am learning to program in Java.");
+        System.out.println("I hope it is a lot of fun!");
+        System.out.println("I hope I get a good grade!");
+         System.out.println("Maybe I'll change my major to computer science.");
+    }
+}
