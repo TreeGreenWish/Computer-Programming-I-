c1baae95c85f3252f21cc86049109e7e3d1c5034
Tyler McCardle <68253755+TreeGreenWish@users.noreply.github.com>
2025-01-30 00:10:17 -0600

    Add files via upload

---
+++ complexfigures.java
@@ -0,0 +1,22 @@
+public class complexfigures {
+public static final int SIZE = 20;
+
+   public static void main (String[] args) {
+      for (int line = 1; line <=SIZE; line++) {
+      
+         for (int slash =1; slash <= (-4 * line + 4 * SIZE); slash++) {
+            System.out.print("/");
+          }  
+         for (int star = 1; star <= (8 * line -8); star++) {
+            System.out.print("*");
+         }
+         for(int slash = 1; slash <=(-4 * line + 4 * SIZE); slash ++) {
+            System.out.print("\\");
+         }
+      
+     System.out.println (); 
+
+      }
+   
+}
+}
